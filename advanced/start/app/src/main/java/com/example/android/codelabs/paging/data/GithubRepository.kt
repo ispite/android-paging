f6bc3b7329d465ff0692760441bac00023e198d0
Denis Solovyov <infinityx93@gmail.com>
2023-04-22 21:14:03 +0300

    on half of point 19 - "Update the paging Flow creation".

--- advanced/start/app/src/main/java/com/example/android/codelabs/paging/data/GithubRepository.kt
+++ advanced/start/app/src/main/java/com/example/android/codelabs/paging/data/GithubRepository.kt
@@ -16,22 +16,31 @@
 
 package com.example.android.codelabs.paging.data
 
+import androidx.paging.ExperimentalPagingApi
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.example.android.codelabs.paging.api.GithubService
+import com.example.android.codelabs.paging.db.RepoDatabase
 import com.example.android.codelabs.paging.model.Repo
 import kotlinx.coroutines.flow.Flow
 
-class GithubRepository(private val service: GithubService) {
+class GithubRepository(private val service: GithubService, private val database: RepoDatabase) {
 
     fun getSearchResultStream(query: String): Flow<PagingData<Repo>> {
+
+        // appending '%' so we can allow other characters to be before and after the query string
+        val dbQuery = "%${query.replace(' ', '%')}%"
+        val pagingSourceFactory = { database.reposDao().reposByName(dbQuery) }
+
+        @OptIn(ExperimentalPagingApi::class)
         return Pager(
             config = PagingConfig(
                 pageSize = NETWORK_PAGE_SIZE,
                 enablePlaceholders = false
             ),
-            pagingSourceFactory = { GithubPagingSource(service, query) }
+            remoteMediator = GithubRemoteMediator(query, service, database),
+            pagingSourceFactory = pagingSourceFactory
         ).flow
     }
 
